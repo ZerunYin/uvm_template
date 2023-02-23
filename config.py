e72eddf2fbc102e7a2037a9d7fde60876430a6e8
Zerun Yin <yinzerun@foxmail.com>
2023-02-23 21:10:31 +0800

    add template

--- config.py
+++ config.py
@@ -1,46 +1,52 @@
 class ComponentConfig(object):
     def __init__(self, name):
         self.name = name
-        self.func_phases = {
-            "build": True,
-            "connect": True,
-            "end_of_elaboration": False,
-            "start_of_simulation": False,
+        self.func_phases = [
+            ("build", True),
+            ("connect", True),
+            ("end_of_elaboration", False),
+            ("start_of_simulation", False),
 
-            "extract": False,
-            "check": False,
-            "report": False,
-            "final": False
-        }
-        self.task_phases = {
-            "run": True,
+            ("extract", False),
+            ("check", False),
+            ("report", False),
+            ("final", False)
+        ]
 
-            "pre_reset": False,
-            "reset": False,
-            "post_reset": False,
-            "pre_configure": False,
-            "configure": False,
-            "post_configure": False,
-            "pre_main": False,
-            "main": False,
-            "post_main": False,
-            "pre_shutdown": False,
-            "shutdown": False,
-            "post_shutdown": False,
-        }
+        self.task_phases = [
+            ("run", True),
+
+            ("pre_reset", False),
+            ("reset", False),
+            ("post_reset", False),
+            ("pre_configure", False),
+            ("configure", False),
+            ("post_configure", False),
+            ("pre_main", False),
+            ("main", False),
+            ("post_main", False),
+            ("pre_shutdown", False),
+            ("shutdown", False),
+            ("post_shutdown", False)
+        ]
         self.fields = {}
         self.rand_fields = {}
+
+        # derived names
+        self.env_name = "%s_%s" % (self.name, "env")
+        self.agent_name = "%s_%s" % (self.name, "agent")
+        self.driver_name = "%s_%s" % (self.name, "driver")
+        self.monitor_name = "%s_%s" % (self.name, "monitor")
+        self.sequencer_name = "%s_%s" % (self.name, "sequencer")
+        self.env_base = "uvm_env"
+        self.agent_base = "uvm_agent"
+        self.driver_base = "uvm_driver"
+        self.monitor_base = "uvm_monitor"
+        self.sequencer_base = "uvm_sequencer"
 
     def __str__(self):
         return self.name
 
 
-class AgentConfig(ComponentConfig):
-    def __init__(self, name):
-        super().__init__(name)
-        self.name += "_agent"
-
-
 if __name__ == '__main__':
-    agent_config = AgentConfig("msg")
-    print(agent_config.name)
+    pass
--- render.py
+++ render.py
@@ -1,16 +1,30 @@
+from os import mkdir, path
 from jinja2 import Environment, FileSystemLoader
-from config import AgentConfig
+from config import ComponentConfig
 
 env = Environment(
     loader=FileSystemLoader(searchpath="templates"),
     trim_blocks=True,
     lstrip_blocks=True
 )
-tpl = env.get_template("agent.sv")
 
-agent_config = AgentConfig("msg")
-args = {
-    "agent_config": agent_config
+# tpls_cfg = {
+#     "env": True,
+#     "agent": True,
+#     "driver": True,
+#     "monitor": True,
+#     "sequencer": True
+# }
+
+tpls_cfg = {
+    "agent": True,
 }
 
-print(tpl.render(**args))
+cfg = ComponentConfig("msg")
+
+for key, value in tpls_cfg.items():
+    if not path.isdir(cfg.name):
+        mkdir(cfg.name)
+    tpl = env.get_template("%s.sv" % key)
+    with open("%s/%s_%s.sv" % (cfg.name, cfg.name, key), "w") as fh:
+        fh.write(tpl.render(name=getattr(cfg, "%s_name" % key), base=getattr(cfg, "%s_base" % key), cfg=cfg))
